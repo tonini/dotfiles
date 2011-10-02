21e610b52d97f828cbb5f8763033c899d939c835
Samuel Tonini <tonini.samuel@gmail.com>
2011-10-02 22:38:14 +0200

    add some emacs environment customization

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(default-frame-alist (quote ((vertical-scroll-bars) (left . 1) (top . 1) (background-color . "black") (background-mode . dark) (border-color . "black") (cursor-color . "#FFFFFF") (foreground-color . "#EDEDED") (mouse-color . "#bcd5fa"))))
+ '(ns-pop-up-frames nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
