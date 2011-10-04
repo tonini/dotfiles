015c8413eb3ca67d0c9a15a2705177195899072a
Samuel Tonini <samuel.tonini@garaio.com>
2011-10-04 08:23:34 +0200

    add some custom emacs variables

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,7 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default-frame-alist (quote ((vertical-scroll-bars) (left . 1) (top . 1) (background-color . "black") (background-mode . dark) (border-color . "black") (cursor-color . "#FFFFFF") (foreground-color . "#EDEDED") (mouse-color . "#bcd5fa"))))
- '(ns-pop-up-frames nil))
+ '(e-max-org-files "~/Dropbox/org/")
+ '(ns-pop-up-frames nil)
+ '(org-clock-out-when-done nil)
+ '(org-clock-persist t)
+ '(org-from-is-user-regexp "\\<samuel tonini\\>")
+ '(org-log-done (quote time)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
