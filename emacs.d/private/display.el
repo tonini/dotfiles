b101da9c4ac42296e88fc3345b6da80b672b9439
Samuel Tonini <tonini.samuel@gmail.com>
2011-10-04 22:36:42 +0200

    update some emacs display stuff

--- emacs.d/private/display.el
+++ emacs.d/private/display.el
@@ -1,3 +1,14 @@
-(set-frame-font "-apple-Bitstream_Vera_Sans_Mono-medium-r-normal--13-130-72-72-m-130-iso10646-1")
+;; current line highlight
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#383737")
+
+;; no blinking cursor
+(if (fboundp 'blink-cursor-mode)
+    (blink-cursor-mode 0))
+
+;; set the cursor type
+(setq default-cursor-type 'box)
+
+(set-frame-font "-apple-Bitstream_Vera_Sans_Mono-medium-r-normal--14-130-72-72-m-130-iso10646-1")
 
 (add-to-list 'default-frame-alist '(background-color . "black"))
