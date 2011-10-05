e56dc3645cb7a8728b9133677bbdb5aaeade9ab6
Samuel Tonini <samuel.tonini@garaio.com>
2011-10-05 11:14:02 +0200

    update emacs region face

--- emacs.d/private/display.el
+++ emacs.d/private/display.el
@@ -1,5 +1,5 @@
 ;; current line highlight color
-(set-face-background 'hl-line "#383737")
+(set-face-background 'hl-line "grey15")
 
 ;; no blinking cursor
 (if (fboundp 'blink-cursor-mode)
@@ -7,6 +7,9 @@
 
 ;; set the cursor type
 (setq default-cursor-type 'box)
+
+(set-face-background 'region "gray38")
+(set-face-foreground 'region "black")
 
 (set-frame-font "-apple-Bitstream_Vera_Sans_Mono-medium-r-normal--14-130-72-72-m-130-iso10646-1")
 
