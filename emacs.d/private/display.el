5b4d6a8b4ad4d97837f769f8a4f85ac56e0267a7
Samuel Tonini <samuel.tonini@garaio.com>
2011-10-10 07:00:20 +0200

    emacs: start with fullscreen

--- emacs.d/private/display.el
+++ emacs.d/private/display.el
@@ -14,3 +14,6 @@
 (set-frame-font "-apple-Bitstream_Vera_Sans_Mono-medium-r-normal--14-130-72-72-m-130-iso10646-1")
 
 (add-to-list 'default-frame-alist '(background-color . "black"))
+
+;; start in full-screen mode
+(ns-toggle-fullscreen)
