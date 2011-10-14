ab72aa4d03f88376d0a0da25d661e736a2f89c8f
Samuel Tonini <tonini.samuel@gmail.com>
2011-10-14 21:24:07 +0200

    emacs: refine custom org-mode functions

--- emacs.d/private/bindings.el
+++ emacs.d/private/bindings.el
@@ -2,6 +2,9 @@
 (e-max-global-set-key (kbd "C-c c") 'mode-compile)
 (e-max-global-set-key (kbd "C-c k") 'mode-compile-kill)
 
+;; org-mode
+(e-max-global-set-key (kbd "C-c t") 'tonini-org-new-project-todo)
+
 ;; buffer navigation
 (e-max-global-set-key (kbd "C-ä") 'forward-buffer)
 (e-max-global-set-key (kbd "C-ö") 'backward-buffer)
--- emacs.d/private/org-mode.el
+++ emacs.d/private/org-mode.el
@@ -1,3 +1,27 @@
+(defvar tonini--org-project-file-path
+  (concat e-max-org-files "projects/"))
+
+(defvar tonini--org-project-files
+  (directory-files tonini--org-project-file-path nil "\\.org$"))
+
+(defun tonini-org-projects-chooser ()
+  (let ((completing-read-func (if (null ido-mode)
+                                  'completing-read
+                                'ido-completing-read)))
+    (setq project-file
+          (funcall completing-read-func
+                   "Project: "
+                   tonini--org-project-files
+                   nil
+                   t))))
+
+(defun tonini-org-project-file ()
+  (concat tonini--org-project-file-path (tonini-org-projects-chooser)))
+
+(defun tonini-org-new-project-todo ()
+  (interactive)
+  (org-capture nil "p"))
+
 (setq org-capture-templates
       '(("p" "Project Todo Files" entry (file+regexp (tonini-org-project-file) "^\*\s*.+\n")
          "** TODO %?\n  %T")))
