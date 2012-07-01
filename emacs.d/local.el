d9340494c1216468f542d4326e0c466e4b4cbc6a
Samuel Tonini <tonini.samuel@gmail.com>
2012-07-01 16:21:17 +0200

    emacs: add jasmine-node compile function

--- emacs.d/local.el
+++ emacs.d/local.el
@@ -3,6 +3,7 @@
   (load (concat (expand-file-name "~/.emacs.d/private/org-mode")))
   (load (concat (expand-file-name "~/.emacs.d/private/bindings")))
   (load (concat (expand-file-name "~/.emacs.d/private/display")))
+  (load (concat (expand-file-name "~/.emacs.d/private/javascript")))
 
   (setq rspec-use-bundler-when-possible t))
 
---
+++ emacs.d/private/javascript.el
@@ -0,0 +1,7 @@
+(defun tonini-jasmine-node-run-single-file (filename)
+  (compile (format "%s %s" "jasmine-node" filename)))
+
+(defun tonini-javascript-mode-hook ()
+  (when (and buffer-file-name (string-match "_spec.js$" buffer-file-name))
+  (setq e-max-testing-execute-function 'tonini-jasmine-node-run-single-file)))
+(add-hook 'js-mode-hook 'tonini-javascript-mode-hook)
