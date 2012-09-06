10efe9761168fe457e64f18d718cef8056139ca8
Samuel Tonini <tonini.samuel@gmail.com>
2012-09-06 22:08:20 +0200

    emacs: add bindings to run ruby files easier

--- emacs.d/private/bindings.el
+++ emacs.d/private/bindings.el
@@ -1,6 +1,8 @@
 ;; compiling
 (cabbage-global-set-key (kbd "C-c c") 'mode-compile)
 (cabbage-global-set-key (kbd "C-c k") 'mode-compile-kill)
+;; ruby compilation
+(cabbage-global-set-key (kbd "C-x M-r") 'ruby-compilation-this-buffer)
 
 ;; org-mode
 (cabbage-global-set-key (kbd "C-c t") 'tonini-org-new-project-todo)
@@ -11,3 +13,6 @@
 
 ;; cua-mode
 (cabbage-global-set-key (kbd "C-c m") 'cua-mode)
+
+;; buffer handling
+(cabbage-global-set-key (kbd "C-x M-k") 'kill-this-buffer)
