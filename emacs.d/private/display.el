f6b5791fc98da6a78505ac900fba2b9a2400f1c0
Samuel Tonini <tonini.samuel@gmail.com>
2013-03-06 21:46:15 +0100

    need to change font size, more work on notebook

--- emacs.d/private/display.el
+++ emacs.d/private/display.el
@@ -7,7 +7,7 @@
 ;; set the cursor type
 (setq default-cursor-type 'box)
 
-(set-frame-font "-apple-Source_Code_Pro-medium-r-normal--15-130-72-72-m-130-iso10646-1")
+(set-frame-font "-apple-Source_Code_Pro-medium-r-normal--13-130-72-72-m-130-iso10646-1")
 
 ;; start in full-screen mode
 (ns-toggle-fullscreen)
