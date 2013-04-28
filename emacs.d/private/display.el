e59526981ba5b52ef52aa505b8c362c71458db14
Samuel Tonini <tonini.samuel@gmail.com>
2013-04-28 11:32:17 +0200

    emacs: updates for display stuff

--- emacs.d/private/display.el
+++ emacs.d/private/display.el
@@ -11,14 +11,19 @@
  '(flymake-warnline ((((class color)) (:background "yellow"))))
  '(org-hide ((t (:foreground "#073642")))))
 
-;; set the cursor type
 (setq default-cursor-type 'box)
 
 (set-frame-font "-apple-Source_Code_Pro-medium-r-normal--13-130-72-72-m-130-iso10646-1")
 
-;; start in full-screen mode
-(ns-toggle-fullscreen)
 (tool-bar-mode -1)
 
 ;; linum-mode setup
 (setq linum-format "%3d ")
+
+(defun tonini-run-after-make-frame-hooks (frame)
+  (select-frame frame)
+  (set-background-color 'black))
+
+(add-hook 'after-make-frame-functions 'tonini-run-after-make-frame-hooks)
+
+(setq color-theme-is-global nil)
