791e5003a7a691036679832c95fede72d47d74da
Samuel Tonini <tonini.samuel@gmail.com>
2013-06-04 22:39:22 +0200

    emacs: elixir-bundle add test runner

--- emacs.d/bundles/elixir/bundle.el
+++ emacs.d/bundles/elixir/bundle.el
@@ -1,3 +1,14 @@
 (cabbage-vendor 'elixir-mode-setup)
 (require 'elixir-mode-setup)
 (elixir-mode-setup)
+
+(defun cabbage-elixir-mode-hook ()
+  (cabbage--set-pairs '("(" "{" "[" "\"" "\'" "|"))
+
+  (defun cabbage-elixir-run-single-file (filename)
+    (compile (format "%s %s" elixir-compiler-command filename)))
+
+  (when (and buffer-file-name (string-match "_test.exs$" buffer-file-name))
+    (setq cabbage-testing-execute-function 'cabbage-elixir-run-single-file)))
+
+(add-hook 'elixir-mode-hook 'cabbage-elixir-mode-hook)
