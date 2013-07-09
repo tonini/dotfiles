f3a99224d0c03460df9ac3136a84f855c2432b96
Samuel Tonini <tonini.samuel@gmail.com>
2013-07-09 16:44:25 +0200

    emacs: clean up display setup

--- emacs.d/private/display.el
+++ emacs.d/private/display.el
@@ -2,32 +2,7 @@
 (if (fboundp 'blink-cursor-mode)
     (blink-cursor-mode 0))
 
-(add-to-list 'default-frame-alist '(background-color . "#000000"))
-(set-cursor-color "#839496")
-
-;; (set-face-background 'modeline-inactive "black")
-;; (set-face-foreground 'vertical-border "black")
-;; (set-face-background 'vertical-border "black")
-(set-frame-parameter nil 'fullscreen 'fullboth)
-
-(custom-set-faces
- '(flymake-errline ((((class color)) (:background "red"))))
- '(flymake-warnline ((((class color)) (:background "yellow"))))
- '(org-hide ((t (:foreground "#073642")))))
-
-(setq default-cursor-type 'box)
-
 (set-frame-font "-apple-Source_Code_Pro-medium-r-normal--13-130-72-72-m-130-iso10646-1")
 
-(tool-bar-mode -1)
-
-;; linum-mode setup
-(setq linum-format "%3d ")
-
-(defun tonini-run-after-make-frame-hooks (frame)
-  (select-frame frame)
-  (set-background-color 'black))
-
-(add-hook 'after-make-frame-functions 'tonini-run-after-make-frame-hooks)
-
-(setq color-theme-is-global nil)
+(set-frame-parameter nil 'fullscreen 'fullboth)
+(setq default-cursor-type 'box)
