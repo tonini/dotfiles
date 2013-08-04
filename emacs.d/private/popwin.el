e4b0929ee22bd54c84cf6551ebd7c7e6fac86a74
Samuel Tonini <tonini.samuel@gmail.com>
2013-08-04 11:01:52 +0200

    emacs: remove popwin

--- emacs.d/private/popwin.el
+++
@@ -1,5 +0,0 @@
-(popwin-mode)
-
-(push '(compilation-mode :noselect nil :height 30) popwin:special-display-config)
-(push '(grep-mode :noselect nil :height 50) popwin:special-display-config)
-(push '("*MIX*" :noselect nil :height 20) popwin:special-display-config)
